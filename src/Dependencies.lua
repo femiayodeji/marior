d5f169ed55aa68132fd3d60679613bccd025b429
Femi Ayodeji <femidotayodeji@gmail.com>
2021-01-04 11:45:51 +0100

    Added sounds, images, frames, and fonts to dependencies

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -11,4 +11,52 @@ require 'src/states/BaseState'
 require 'src/Animation'
 
 
+gSounds = {
+    ['jump'] = love.audio.newSource('sounds/jump.wav', 'static'),
+    ['death'] = love.audio.newSource('sounds/death.wav', 'static'),
+    ['music'] = love.audio.newSource('sounds/music.wav', 'static'),
+    ['powerup-reveal'] = love.audio.newSource('sounds/powerup-reveal.wav', 'static'),
+    ['pickup'] = love.audio.newSource('sounds/pickup.wav', 'static'),
+    ['empty-block'] = love.audio.newSource('sounds/empty-block.wav', 'static'),
+    ['kill'] = love.audio.newSource('sounds/kill.wav', 'static'),
+    ['kill2'] = love.audio.newSource('sounds/kill2.wav', 'static')
+}
+
+gTextures = {
+    ['tiles'] = love.graphics.newImage('graphics/tiles.png'),
+    ['toppers'] = love.graphics.newImage('graphics/tile_tops.png'),
+    ['bushes'] = love.graphics.newImage('graphics/bushes_and_cacti.png'),
+    ['jump-blocks'] = love.graphics.newImage('graphics/jump_blocks.png'),
+    ['gems'] = love.graphics.newImage('graphics/gems.png'),
+    ['backgrounds'] = love.graphics.newImage('graphics/backgrounds.png'),
+    ['green-alien'] = love.graphics.newImage('graphics/green_alien.png'),
+    ['creatures'] = love.graphics.newImage('graphics/creatures.png')
+}
+
+gFrames = {
+    ['tiles'] = GenerateQuads(gTextures['tiles'], TILE_SIZE, TILE_SIZE),
+    
+    ['toppers'] = GenerateQuads(gTextures['toppers'], TILE_SIZE, TILE_SIZE),
+    
+    ['bushes'] = GenerateQuads(gTextures['bushes'], 16, 16),
+    ['jump-blocks'] = GenerateQuads(gTextures['jump-blocks'], 16, 16),
+    ['gems'] = GenerateQuads(gTextures['gems'], 16, 16),
+    ['backgrounds'] = GenerateQuads(gTextures['backgrounds'], 256, 128),
+    ['green-alien'] = GenerateQuads(gTextures['green-alien'], 16, 20),
+    ['creatures'] = GenerateQuads(gTextures['creatures'], 16, 16)
+}
+
+gFrames['tilesets'] = GenerateTileSets(gFrames['tiles'], 
+    TILE_SETS_WIDE, TILE_SETS_TALL, TILE_SET_WIDTH, TILE_SET_HEIGHT)
+
+gFrames['toppersets'] = GenerateTileSets(gFrames['toppers'], 
+    TOPPER_SETS_WIDE, TOPPER_SETS_TALL, TILE_SET_WIDTH, TILE_SET_HEIGHT)
+
+gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['large'] = love.graphics.newFont('fonts/font.ttf', 32),
+    ['title'] = love.graphics.newFont('fonts/ArcadeAlternate.ttf', 32)
+}
+
 
