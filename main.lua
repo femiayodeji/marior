8b759497d004ec4dc10969ed116595b51709d254
Femi Ayodeji <femidotayodeji@gmail.com>
2021-01-10 00:21:52 +0100

    Completed main script

--- main.lua
+++ main.lua
@@ -27,4 +27,31 @@ function love.load()
     love.keyboard.keysPressed = {}
 end
 
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then 
+        love.event.quit()
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:start()
+    gStateMachine:render()
+    push:finish()
+end
 
