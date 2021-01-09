9ac4077f8d17c0c9d0af542bafcdb3825e301702
Femi Ayodeji <femidotayodeji@gmail.com>
2021-01-09 23:18:03 +0100

    Added game load functionalities

--- main.lua
+++ main.lua
@@ -1,2 +1,30 @@
 love.graphics.setDefaultFilter('nearest', 'nearest')
 require 'src/Dependencies'
+
+function love.load()
+    love.graphics.setFont(gFonts['medium'])
+    love.window.setTitle('Marior - Super Mario Bros.')
+
+    math.randomseed(os.time())
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = true,
+        canvas
+    })
+
+    gStateMachine = StateMachine{
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end
+    }
+    gStateMachine:change('start')
+
+    gSounds['music']:setLooping(true)
+    gSounds['music']:setVolume(0.5)
+    gSounds['music']:play()
+
+    love.keyboard.keysPressed = {}
+end
+
+
