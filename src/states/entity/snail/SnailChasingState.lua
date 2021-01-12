292c2dd65a040a58c70c9f3ce0efc34732dde630
Femi Ayodeji <femidotayodeji@gmail.com>
2021-01-12 14:12:04 +0100

    Fixed snail chasing state

--- src/states/entity/snail/SnailChasingState.lua
+++ src/states/entity/snail/SnailChasingState.lua
@@ -25,7 +25,7 @@ function SnailChasingState:update(dt)
         local tileBottomLeft = self.tilemap:pointToTile(self.snail.x, self.snail.y + self.snail.height)
 
         if (tileLeft and tileBottomLeft) and (tileLeft:collidable() or not tileBottomLeft:collidable()) then 
-            self.snail.x = self.snail.x - SNAIL_MOVE_SPEED * dt 
+            self.snail.x = self.snail.x + SNAIL_MOVE_SPEED * dt 
         end
     else
         self.snail.direction = 'right'
